34c4685ae1c6db8a052d3fca618410cab0823b87
junade <junade@netwarriors.co.za>
2021-04-22 15:41:13 +0200

    Add humidity data to weather details

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,12 +4,13 @@ const forecast = (latitude, longitude, callback) => {
   const url = 'http://api.weatherstack.com/current?access_key=b479c713066c0ac4f6ba32df8b1bee3c&query=' + latitude + ',' + longitude + '&units=m'
 
   request({ url, json: true }, (error, { body }) => {
+    console.log(body.current)
     if (error) {
       callback('Unable to connect to weather service!', undefined)
     } else if (body.error) {
       callback('Unable to find location', undefined)
     } else {
-      callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out.")
+      callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out. The humidity is currently " + body.current.humidity + "%.")
     }
   })
 }
